fb3c1f99e2b316151dae9da518177168fd5046a8
HotDiddlyDog <kathyy.nguyen121@gmail.com>
2023-10-24 20:29:49 -0500

    define box

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,9 +3,8 @@ function Intro(){
     return(
         <>
         <div class="container-intro">
-            <div class='container-introcontent'>
                 <img class="intro-img"src={require("../assets/scenery.jpg")}alt="intro"></img>
-                <div>
+                <div class='container-introcontent'>
                     <h2>Kathy</h2>
                     <p>My name is Kathy. I have a passion for blah blah blah with interests in blah blag 
                     I have a passion for blah blah blah with interests in blah blag bla
@@ -18,7 +17,6 @@ function Intro(){
                     I have a passion for blah blah blah with interests in blah blag bla
                     I have a passion for blah blah blah with interests in blah blag blablah.</p>
                 </div>
-            </div>
         </div>
         </>
     );
