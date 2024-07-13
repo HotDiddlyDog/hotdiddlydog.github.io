6ab9878d588483310ab5e767e27439794180a481
HotDiddlyDog <kathyy.nguyen121@gmail.com>
2024-07-13 12:24:58 -0500

    update project 1

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,34 +1,43 @@
-import Card from "./Card.js";
-import "../assets/coverbanner.jpg";
+import React from 'react';
+import Card from './Card.js';
+import '../assets/coverbanner.jpg';
 
-function Body(){
-    return(
+function Body() {
+    return (
         <>
-        <div class='outer-body'>
-        <h1>Projects</h1>
-        <div class="container-body">
-            <div class="project">
-                <img className="info-picture"src={require("../assets/coverbanner.jpg")} alt="cat"/>
-                <Card  title="To be updated" description="To be updated " />
+            <div className="outer-body">
+                <h1>Projects</h1>
+                <div className="container-body">
+                    <div className="project">
+                        <video className="info-video" controls>
+                            <source src={require('../assets/pitch video.mp4')} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                        <Card
+                            title="False Alarm Processing App"
+                            description="This application was created in order to replace an older application 
+                            that was in use at the time. In this video, no real people are used. Only for entertainment 
+                            purposes and for showcasing the unfinished application. No real data was used. Repo is private for security purposes. 
+                            Video made by me :)"
+                        />
+                    </div>
+                    <div className="project">
+                        <img className="info-picture" src={require('../assets/scenery.jpg')} alt="cat" />
+                        <Card title="To be updated" description="To be updated " />
+                    </div>
+                    <div className="project">
+                        <img className="info-picture" src={require('../assets/coverbanner.jpg')} alt="cat" />
+                        <Card title="To be updated" description="To be updated " />
+                    </div>
+                    <div className="project">
+                        <img className="info-picture" src={require('../assets/scenery.jpg')} alt="cat" />
+                        <Card title="To be updated" description="To be updated" />
+                    </div>
+                </div>
             </div>
-            <div class="project">
-                <img className="info-picture"src={require("../assets/scenery.jpg")}alt="cat"/>
-                <Card  title="To be updated" description="To be updated " />
-            </div>
-            <div class="project">
-                <img className="info-picture"src={require("../assets/coverbanner.jpg")} alt="cat"/>
-                <Card  title="To be updated" description="To be updated " />
-            </div>
-            <div class="project">
-                <img className="info-picture"src={require("../assets/scenery.jpg")}alt="cat"/>
-                <Card  title="To be updated" description="To be updated" />
-            </div>
-            
-        </div>
-        </div>
-        <hr class='break'></hr>
-        
+            <hr className="break"></hr>
         </>
     );
 }
+
 export default Body;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,13 @@
+import React from 'react';
+
 function Card(props) {
-    return(
-        <div>
+    return (
+        <div className="card-content">
             <h2>{props.title}</h2>
-            <p>{props.description}</p>
+            <p className="card-description">{props.description}</p>
             {/* <button>View Project</button> */}
         </div>
     );
 }
+
 export default Card;
